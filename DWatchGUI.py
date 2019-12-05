8bdcd78041f92ac11875319d4414bf3b757d7403
jps <jpsalo@zoho.com>
2019-12-05 18:43:25 +0200

    Initial work for time display and edit

--- DWatchGUI.py
+++ DWatchGUI.py
@@ -2,17 +2,21 @@ from Tkinter import *
 from LowLevelGUI import *
 import time
 
+is_bottom_right_pressed = True
+
 class DWatchGUI:
   def __init__(self, parent, eventhandler):
     self.GUI = LowLevelGUI(parent, self)
 
     self.eventhandler = eventhandler
 
+    self.parent = parent
+
     self.handleEventOn
-  
+
   def handleEventOn(self):
     self.eventhandler.event("on")
-  
+
   def wait(self):
     self.eventhandler.event("lightOff2")
     print "wait"
@@ -31,25 +35,38 @@ class DWatchGUI:
   def topRightReleased(self):
     self.eventhandler.event("lightOff")
     print "topRightReleased"
-  
+
   def topLeftPressed(self):
     self.eventhandler.event("changeMode")
-  
+    self.eventhandler.event('selectNext')
+
   def topLeftReleased(self):
     print "topLeftReleased"
-    
+
   def bottomRightPressed(self):
     self.eventhandler.event("initChrono")
-    self.eventhandler.event("editTime")
+    self.maybeEditTime()
     self.eventhandler.event("finishEdit")
 
+
+  def maybeEditTime(self):
+    self.is_bottom_right_pressed = True
+    self.parent.after(1500, self.tryActivateEditTime)
+
+
+  def tryActivateEditTime(self):
+    if self.is_bottom_right_pressed:
+        self.eventhandler.event("editTime")
+
+
   def bottomRightReleased(self):
+    self.is_bottom_right_pressed = False
     self.eventhandler.event("released")
-  
+
   def bottomLeftPressed(self):
     self.eventhandler.event("resetChrono")
     self.eventhandler.event("increase")
-    self.eventhandler.event("setAlarm")  
+    self.eventhandler.event("setAlarm")
 
   def bottomLeftReleased(self):
     self.eventhandler.event("stopInc")
@@ -76,51 +93,51 @@ class DWatchGUI:
 
   def refreshAlarmDisplay(self):
     self.GUI.drawAlarm()
- 
+
   def increaseTimeByOne(self):
     self.GUI.increaseTimeByOne()
-    self.refreshTimeDisplay()    
- 
+    self.refreshTimeDisplay()
+
   def resetChrono(self):
     self.GUI.resetChrono()
-    
+
   def increaseChronoByOne(self):
     self.GUI.increaseChronoByOne()
-    
+
   #Select current display:
-  
+
   def startSelection(self):
     self.GUI.startSelection()
-    
+
   def selectNext(self):
-    self.GUI.selectNext() 
-       
-  #Modify the state corresponing to the selection 
+    self.GUI.selectNext()
+
+  #Modify the state corresponing to the selection
   def increaseSelection(self):
     self.GUI.increaseSelection()
-        
+
   def stopSelection(self):
     self.GUI.stopSelection()
-                    
-         
+
+
   #Light / Alarm:
-  
+
   def setIndiglo(self):
     self.GUI.setIndiglo()
-    
+
   def unsetIndiglo(self):
     self.GUI.unsetIndiglo()
-    
+
   def setAlarm(self):
     self.GUI.setAlarm()
 
-  # Query 
+  # Query
   def getTime(self):
     return self.GUI.getTime()
 
   def getAlarm(self):
     return self.GUI.getAlarm()
-     
+
   #Check if time = alarm set time
   def checkTime(self):
     if self.GUI.getTime()[0] == self.GUI.getAlarm()[0] and self.GUI.getTime()[1] == self.GUI.getAlarm()[1] and self.GUI.getTime()[2] == self.GUI.getAlarm()[2]:
